b008e092a1c03eb527bdff4ebfb7e6feeab9cc00
Z0b <patel.panth@outlook.com>
2021-11-12 03:10:13 +0530

    wheelencoder

---
+++ wheel_encoder.py
@@ -0,0 +1,16 @@
+import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
+import time
+GPIO.setwarnings(False) # Ignore warning for now
+GPIO.setmode(GPIO.BCM) # Use physical pin numbering
+GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+# Set pin 10 to be an input pin and set initial value to be pulled low (off)
+print("start")
+curr=0
+prev=0
+count=0
+while True: # Run forever
+    curr=GPIO.input(4)
+    if curr==1 and prev==0:
+        count+=1
+    prev=curr
+        
